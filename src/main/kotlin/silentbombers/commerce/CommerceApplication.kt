a50c526ab4488321bfeefe9230d7462c4930ce58
soominsohn <soomin.sohn.dev@gmail.com>
2023-05-29 17:24:19 +0900

    initial commit

---
+++ src/main/kotlin/silentbombers/commerce/CommerceApplication.kt
@@ -0,0 +1,11 @@
+package silentbombers.commerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommerceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CommerceApplication>(*args)
+}
---
+++ src/test/kotlin/silentbombers/commerce/CommerceApplicationTests.kt
@@ -0,0 +1,13 @@
+package silentbombers.commerce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommerceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
